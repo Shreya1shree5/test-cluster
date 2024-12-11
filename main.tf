17b78e28373805a8580f647e74ec3f92756d6c81
Shreya1shree5 <47668807+Shreya1shree5@users.noreply.github.com>
2024-12-11 15:56:58 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,96 @@
+resource "google_container_cluster" "primary" {
+  name               = var.cluster_name
+  location           = var.region
+  initial_node_count = 2
+
+  node_config {
+    machine_type = var.machine_type
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+
+  network    = var.network
+  subnetwork = var.subnetwork
+
+  autoscaling {
+    enabled = true
+    min_node_count = 1
+    max_node_count = 5
+  }
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  cluster    = google_container_cluster.primary.name
+  location   = google_container_cluster.primary.location
+  node_count = 2
+
+  node_config {
+    machine_type = var.machine_type
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 5
+  }
+}
+
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "nginx-deployment"
+    labels = {
+      app = "nginx"
+    }
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app = "nginx"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "nginx"
+        }
+      }
+
+      spec {
+        container {
+          name  = "nginx"
+          image = "nginx:1.17.10"
+
+          ports {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx-service"
+  }
+
+  spec {
+    selector = {
+      app = "nginx"
+    }
+
+    type = "LoadBalancer"
+
+    port {
+      port        = 80
+      target_port = 80
+    }
+  }
+}
