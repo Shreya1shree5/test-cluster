f0c5dfdce16c46b4883932c0d3b8fd4928c31ea8
Shreya1shree5 <47668807+Shreya1shree5@users.noreply.github.com>
2024-12-11 20:45:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,3 @@
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
-
-provider "google-beta" {
-  project = var.project_id
-  region  = var.region
-}
-
-provider "kubernetes" {
-  host                   = google_container_cluster.primary.endpoint
-  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
-  token                  = google_container_cluster.primary.master_auth.0.token
-  load_config_file       = false
-}
-
 resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.region
