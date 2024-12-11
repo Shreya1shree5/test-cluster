5547d366fbd6e64ad6974759b6c87b99ba3b8061
Shreya1shree5 <47668807+Shreya1shree5@users.noreply.github.com>
2024-12-11 20:44:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,36 @@
-resource "google_container_cluster" "primary" {
-  name               = var.cluster_name
-  location           = var.region
-  initial_node_count = 2
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
 
-  node_config {
-    machine_type = var.machine_type
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform",
-    ]
-  }
+provider "google-beta" {
+  project = var.project_id
+  region  = var.region
+}
+
+provider "kubernetes" {
+  host                   = google_container_cluster.primary.endpoint
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+  token                  = google_container_cluster.primary.master_auth.0.token
+  load_config_file       = false
+}
+
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.region
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
 
   network    = var.network
   subnetwork = var.subnetwork
 }
 
 resource "google_container_node_pool" "primary_nodes" {
-  cluster    = google_container_cluster.primary.name
-  location   = google_container_cluster.primary.location
+  cluster  = google_container_cluster.primary.name
+  location = google_container_cluster.primary.location
+
+  node_count = 2
 
   node_config {
     machine_type = var.machine_type
@@ -86,4 +100,39 @@ resource "kubernetes_service" "nginx" {
       target_port = 80
     }
   }
+}
+
+variable "project_id" {
+  description = "The Google Cloud project ID."
+  type        = string
+}
+
+variable "region" {
+  description = "The region to deploy the GKE cluster in."
+  type        = string
+  default     = "us-central1"
+}
+
+variable "cluster_name" {
+  description = "The name of the GKE cluster."
+  type        = string
+  default     = "my-gke-cluster"
+}
+
+variable "machine_type" {
+  description = "The machine type for the cluster nodes."
+  type        = string
+  default     = "e2-medium"
+}
+
+variable "network" {
+  description = "The VPC network to use for the GKE cluster."
+  type        = string
+  default     = "default"
+}
+
+variable "subnetwork" {
+  description = "The subnetwork to use for the GKE cluster."
+  type        = string
+  default     = "default"
 }
